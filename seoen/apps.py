411aa3c98cf0216b51d8cec96295e7e80a6006b3
kkutayozgun <kkutayozgun@gmail.com>
2021-10-02 15:50:37 +0300

    admin degisiklikler

--- seoen/apps.py
+++ seoen/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class SeoenConfig(AppConfig):
     name = 'seoen'
+    verbose_name = "Maxident - EN"
--- seotr/apps.py
+++ seotr/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class SeotrConfig(AppConfig):
     name = 'seotr'
+    verbose_name = "Maxident - TR"
